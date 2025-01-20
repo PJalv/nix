77669a6e3b0d1a6e4fedf974abca466ba287f6f5
PJalv <pjalvbusiness@gmail.com>
2025-01-19 20:10:19 -0800

    tweak

---
+++ users/pjalv/laptop/hm/entries.nix
@@ -0,0 +1,15 @@
+
+{ ... }: {
+
+xdg.desktopEntries = {
+   stm32cubemx = {
+    name = "STM32CubeMX";
+    genericName = "STM32CubeMX";
+    exec = "stm32cubemx";
+    terminal = false;
+    categories = [ "Application" ];
+  };
+
+  };
+
+}
