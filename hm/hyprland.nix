0634bc62423b528332d4e7120417c6b8f9eac9dd
PJalv <pjalvbusiness@gmail.com>
2025-03-06 17:22:22 -0800

    Tweak

--- hm/hyprland.nix
+++ hm/hyprland.nix
@@ -190,7 +190,7 @@
       exec-once = fusuma
       exec-once = swww img "$(find -L .config/wallpaper -type f \( -iname '*.jpg' -o -iname '*.png' -o -iname '*.jpeg' \) | shuf -n 1)"
       exec-once = cd $($HOME)/.config/waybar && nix-shell --run "uv run python main.py" 
-      exec-once = /home/${username}/.config/waybar/setup.sh 
+      exec-once = /home/${username}/.config/styles/setup.sh 
 
       xwayland {
         force_zero_scaling = true
