2fafdcbf4323ef43b2fa36baa7f27f247eb1ffc0
PJalv <pjalvbusiness@gmail.com>
2025-07-05 19:37:31 -0700

    Tweak

--- hm/firefox.nix
+++ hm/firefox.nix
@@ -119,12 +119,6 @@
     };
   };
 
-  home = {
-    persistence = {
-      # Not persisting is safer
-      # "/persist/${config.home.homeDirectory}".directories = [ ".mozilla/firefox" ];
-    };
-  };
 
   xdg.mimeApps.defaultApplications = {
     "text/html" = ["firefox.desktop"];
