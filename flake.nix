823bc947a4ffbc3d50b631a0b9055dc71cba8c8e
Jorge Luis Suarez <jorge.suarez@tp-link.com>
2025-11-03 13:45:35 -0800

    Tweak

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
     opencode-flake = {
-      url = "github:spahl/opencode-flake";
+      url = "github:PJalv/opencode-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
