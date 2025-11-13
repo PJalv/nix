87575a6be801dd2d1a9bae763686d82e337a067f
PJalv <pjalvbusiness@gmail.com>
2025-11-12 20:32:47 -0800

    Tweak

--- users/pjalv/user.nix
+++ users/pjalv/user.nix
@@ -233,8 +233,13 @@ in {
     (lib.mkIf (machine == "laptop") {
       services = {
         displayManager.sddm = {
+          package = pkgs.kdePackages.sddm;
+          extraPackages = with pkgs; [
+          kdePackages.qt5compat
+          ];
           enable = true;
           theme = "catppuccin-sddm-corners";
+          wayland.enable = true;
         };
         power-profiles-daemon.enable = true;
         libinput.enable = true;
