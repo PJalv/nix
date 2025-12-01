d3626eb0e69eb9e7920c8fda1184cf9b4d7d6fc3
PJalv <pjalvbusiness@gmail.com>
2025-11-30 20:18:57 -0800

    Tweak

--- hm/hyprlock.nix
+++ hm/hyprlock.nix
@@ -25,7 +25,7 @@ in {
       background {
           monitor =
           #path = screenshot
-          path = cmd[] echo ${dotfilesDir}
+          path = cmd[] find ${dotfilesDir}/.config/wallpaper/ -type f \( -iname '*.jpg' -o -iname '*.png' \) | shuf -n 1
           #color = $background
           blur_passes = 2
           contrast = 1
