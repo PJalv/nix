60ce27bacef3abc2f0384c099ef3c7183ced4c98
Jorge Luis Suarez <jorge.suarez@tp-link.com>
2025-12-04 10:17:57 -0800

    Tweak

--- flake.nix
+++ flake.nix
@@ -18,16 +18,8 @@
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # opencode-flake = {
-    #   url = "github:spahl/opencode-flake/d98f05cda2758feeb9fe1c8d2a13d417c6a29fc5";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
     opencode-flake = {
       url = "github:sst/opencode";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    openspec-flake = {
-      url = "github:spahl/opencode-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
