f5c2e3d2d858c95e9ed997448648660d28e3ebb8
Jorge Luis Suarez <jorge.suarez@tp-link.com>
2025-12-17 13:48:27 -0800

    Tweak

--- users/remote/hm.nix
+++ users/remote/hm.nix
@@ -29,6 +29,7 @@ in {
     alejandra
     tree-sitter
     inputs.opencode-flake.packages.${pkgs.system}.default
+    inputs.opencode-flake.packages.${pkgs.system}.openspec
   ];
   imports = [../../hm/zsh.nix ../../hm/starship.nix];
 
