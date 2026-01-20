58873b251386c732ad02e88be2c833f6bcfa9660
PJalv <pjalvbusiness@gmail.com>
2026-01-20 10:01:23 -0800

    Tweak

--- users/pjalv/hm.nix
+++ users/pjalv/hm.nix
@@ -87,8 +87,8 @@ in {
 
 
   programs.chromium = {
-    package = pkgs.ungoogled-chromium;
-    # package = pkgs.chromium;
+    #package = pkgs.ungoogled-chromium;
+    package = pkgs.chromium;
     enable = true;
     extensions = [
       {id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";} # ublock origin
